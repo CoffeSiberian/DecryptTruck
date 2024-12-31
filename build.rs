51827321f2edfcb230141efa335b8e5a70c50ce9
Fernando Garrido <siberiancoffe@outlook.cl>
2024-12-30 21:40:15 -0300

    adjusted the build to ignore the parameters if it is another project and not alter external projects

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if cfg!(target_os = "windows") {
+    if cfg!(target_os = "windows") && std::env::var("CARGO_PKG_NAME").unwrap() == "DecryptTruck" {
         let mut res = winres::WindowsResource::new();
         res.set("FileDescription", env!("CARGO_PKG_DESCRIPTION"));
         res.set("ProductName", env!("CARGO_PKG_NAME"));
