2c9ed72975972a7ff9f8b6700c373593b8b1774a
Fernando Garrido <siberiancoffe@outlook.cl>
2024-12-30 21:57:47 -0300

    fix: Correct package name in build script for Windows and bump version to 1.3.1

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if cfg!(target_os = "windows") && std::env::var("CARGO_PKG_NAME").unwrap() == "DecryptTruck" {
+    if cfg!(target_os = "windows") && std::env::var("CARGO_PKG_NAME").unwrap() == "decrypt_truck" {
         let mut res = winres::WindowsResource::new();
         res.set("FileDescription", env!("CARGO_PKG_DESCRIPTION"));
         res.set("ProductName", env!("CARGO_PKG_NAME"));
