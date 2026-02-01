52b175cfc59e51aa46632df047d6ad136809883e
Fernando Garrido <siberiancoffe@outlook.cl>
2026-02-01 20:42:41 -0300

    A new test was created for modifying custom types

--- tests/decrypt_bin_file_test.rs
+++ tests/decrypt_bin_file_test.rs
@@ -39,3 +39,16 @@ fn test_decrypt_bin_encrypted_file_with_mod() {
         "Decrypted file with mod is not the same as the original file"
     );
 }
+
+#[test]
+fn test_decrypt_bin_encrypted_file_with_mod_2() {
+    let file_bin = read_file_bin("tests/test_data/encrypted_sig/game_with_mod_2.sii").unwrap();
+    let decrypted_file = decrypt_bin_file(&file_bin).unwrap();
+
+    let file_compare = read_file_bin("tests/test_data/plain_text_sig/game_with_mod_2.sii").unwrap();
+
+    assert!(
+        decrypted_file == file_compare,
+        "Decrypted file with mod is not the same as the original file"
+    );
+}
